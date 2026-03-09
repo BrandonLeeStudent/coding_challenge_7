f3c35f3378372b5f7e10f671e00813ca32b99505
BrandonLeeStudent <bklee2@usf.edu>
2026-03-08 20:40:34 -0400

    commit

--- cc_7.js
+++ cc_7.js
@@ -2,11 +2,10 @@ const input = document.getElementById("cookieOrder");
 const button = document.getElementById("orderButton");
 const headline = document.getElementById("PlaceOrderHeadline");
 
-// Add event listener
 button.addEventListener("click", function() {
     const orderText = input.value;
 
     if (orderText !== "") {
         headline.textContent = orderText;
     }
-});
+})
